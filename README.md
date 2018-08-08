92ad852d05e015b2a4c32f333b30dba35fca4d29
e4rache <e4rache@protonmail.com>
2018-08-08 07:50:14 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# radio-jam-front-end
+** Radio Jam **
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Radio Jam sr & jr</h1>
+        </header>
+        <p className="App-intro">coming soon ...</p>
+      </div>
+    );
+  }
+}
+
+export default App;
